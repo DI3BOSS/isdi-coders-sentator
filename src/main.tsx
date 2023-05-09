8955c53d86a35e744d7e08c555ea0b3dce8962b2
DI3BOSS <diegofbermudez@gmail.com>
2023-05-09 20:27:56 +0200

    Create GlobalStyles using styled-components and refactor style resets

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import App from "./components/App/App";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import GlobalStyles from "./styles/GlobalStyles";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
+      <GlobalStyles />
       <App />
     </Provider>
   </React.StrictMode>
