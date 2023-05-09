61bb88220972fb80847b6f026588a3b13d8b90f8
DI3BOSS <diegofbermudez@gmail.com>
2023-05-09 11:04:55 +0200

    Implement the Button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,10 @@
+interface ButtonProps {
+  actionOnClick: () => void;
+  text: string;
+}
+
+const Button = ({ actionOnClick, text }: ButtonProps): JSX.Element => {
+  return <button onClick={actionOnClick}>{text}</button>;
+};
+
+export default Button;
