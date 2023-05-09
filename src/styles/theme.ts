082029ae992b67910727a68c222ec0cb1ed3ba5c
DI3BOSS <diegofbermudez@gmail.com>
2023-05-09 20:57:19 +0200

    Implement a default theme for the app and add a ThemeProvider to main

---
+++ src/styles/theme.ts
@@ -0,0 +1,23 @@
+import { DefaultTheme } from "styled-components";
+
+const theme: DefaultTheme = {
+  colors: {
+    primary: "rgba(26, 26, 26, 1)",
+    secondary: "rgba(240, 248, 255, 1)",
+    dark: "rgba(40, 160, 201, 1)",
+    contrast: "rgba(255, 215, 0, 1)",
+    light: "rgba(240, 255, 255, 1)",
+    black: "rgba(0, 0, 0, 1)",
+    white: "rgba(255, 255, 255, 1)",
+  },
+  fonts: {
+    mainFont: "sans-serif",
+    titleFont: "sans-serif",
+  },
+  fontSize: {
+    mainFont: "1rem",
+    titleFont: "2rem",
+  },
+};
+
+export default theme;
