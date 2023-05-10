84b016063131441bba14f61419e4e334f5b9fa6d
DI3BOSS <diegofbermudez@gmail.com>
2023-05-11 00:14:16 +0200

    Refactor Button to accept className as props

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,21 @@
 import ButtonStyled from "./ButtonStyled";
 
 interface ButtonProps {
-  actionOnClick: () => void;
-  text: string;
+  actionOnClick?: () => void;
+  text?: string;
+  className?: string;
 }
 
-const Button = ({ actionOnClick, text }: ButtonProps): JSX.Element => {
-  return <ButtonStyled onClick={actionOnClick}>{text}</ButtonStyled>;
+const Button = ({
+  actionOnClick,
+  text,
+  className,
+}: ButtonProps): JSX.Element => {
+  return (
+    <ButtonStyled onClick={actionOnClick} className={className}>
+      {text}
+    </ButtonStyled>
+  );
 };
 
 export default Button;
