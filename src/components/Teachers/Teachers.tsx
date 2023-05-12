9c7455e889bb0acdfaac938e1570aaae852b98e3
DI3BOSS <diegofbermudez@gmail.com>
2023-05-13 00:29:16 +0200

    Refactor Teachers component adding content and structure

--- src/components/Teachers/Teachers.tsx
+++ src/components/Teachers/Teachers.tsx
@@ -1,5 +1,25 @@
-const Teachers = (): JSX.Element => {
-  return <></>;
+import Image from "../Image/Image";
+import TeachersStyled from "./TeachersStyled";
+
+interface TeachersProps {
+  teachers: string[];
+}
+const Teachers = ({ teachers }: TeachersProps): JSX.Element => {
+  return (
+    <TeachersStyled>
+      {teachers.map((teacher) => {
+        const teacherName = teacher.replace(/-/g, " ");
+        return (
+          <Image
+            className="teacher-image"
+            src={`assets/images/teachers_photos/${teacher}.png`}
+            alt={`teacher ${teacherName}`}
+            key={teacher}
+          />
+        );
+      })}
+    </TeachersStyled>
+  );
 };
 
 export default Teachers;
